c748cbd5c549fe09a1cc785409fb2ca890c1d90c
Vannu25 <92512520+Vannu25@users.noreply.github.com>
2021-11-12 05:52:46 -0800

    Add files via upload

---
+++ SpringOrm5Application.java
@@ -0,0 +1,13 @@
+package com.example.nsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrm5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrm5Application.class, args);
+	}
+
+}
